8188ceacee66554bba3924e67facce21b58f2d07
x86q <abhishekwl@protonmail.com>
2021-04-05 22:06:52 +0530

    Added play function

--- src/thesaurus/controller/play.controller.js
+++ src/thesaurus/controller/play.controller.js
@@ -1,11 +1,9 @@
 const { program } = require('commander');
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const prompt = require('prompt-sync')({ sigint: true });
 const fs = require('fs');
 const COMMAND_ENUM = require('../enums/command.enum');
-const { getCommandDescription, rectifyAndDisplaySentence } = require('../../util');
+const { getCommandDescription } = require('../../util');
+const fullDictController = require('./full-dict.controller');
 const { getThesaurusData } = require('../services/thesaurus.service');
 const randomWords = fs.readFileSync('./src/shared/random.words.txt', 'utf-8').split('\n');
 
@@ -14,32 +12,62 @@ playCommand
     .description(getCommandDescription(COMMAND_ENUM.PLAY))
     .action(onWordInput);
 
-async function game(
-    randomResourceIndex,
-    selectedResource,
-    selectedResourceIndex,
-    word,
-    resourcesArr,
-) {
+async function userWordInput(word, resourcesArr) {
+    const userWord = prompt('Guess the word: ').trim();
+    if (userWord === word || resourcesArr[1].includes(userWord)) {
+        console.log('Congratulations! You guessed it right!');
+        process.exit(0);
+    }
+    else await secondAttempt(word, resourcesArr);
+}
+
+async function secondAttempt(word, resourcesArr) {
+    console.log('Wrong answer\n');
+    console.log('1. Try again');
+    console.log('2. Hint');
+    console.log('3. Quit');
+    const userInput = Number(prompt('Enter your choice: ').trim());
+    switch (userInput) {
+        case 1:
+            await userWordInput(word, resourcesArr);
+            break;
+
+        case 2:
+            displayHint(resourcesArr);
+            await userWordInput(word, resourcesArr);
+            break;
+        
+        case 3:
+            console.log(`The word is ${word}.`);
+            await fullDictController.onWordInput(word);
+            process.exit(0);
+    }
+}
+
+function displayHint(resourcesArr) {
+    const randomResourceIndex = Math.floor(Math.random() * resourcesArr.length);
+    const selectedResource = resourcesArr[randomResourceIndex];
+    const selectedResourceIndex = 0;
+    if (selectedResourceIndex >= selectedResource.length) selectedResourceIndex = 0;
+    if (selectedResource.length > 0 && selectedResourceIndex < selectedResource.length) {
+        switch (randomResourceIndex) {
+            case 0:
+                console.log(`Definition: ${resourcesArr[0][selectedResourceIndex]}`);
+                break;
+            case 1:
+                console.log(`Synonym: ${resourcesArr[1][selectedResourceIndex]}`);
+                break;
+            default:
+                console.log(`Antonym: ${resourcesArr[2][selectedResourceIndex]}`);
+                break;
+        }
+    }
+}
+
+async function game(word, resourcesArr) {
     while (true) {
-        if (selectedResource.length > 0 && selectedResourceIndex < selectedResource.length) {
-            switch (randomResourceIndex) {
-                case 0:
-                    console.log(`Definition: ${resourcesArr[0][selectedResourceIndex]}`);
-                    break;
-                case 1:
-                    console.log(`Synonym: ${resourcesArr[1][selectedResourceIndex]}`);
-                    break;
-                default:
-                    console.log(`Antonym: ${resourcesArr[2][selectedResourceIndex]}`);
-                    break;
-            }
-        } else selectedResourceIndex = 0;
-        readline.question('Guess the word', userWord => {
-            console.log('You said '+userWord);
-            readline.close();
-        });
-        selectedResourceIndex++;
+        displayHint(resourcesArr);
+        await userWordInput(word, resourcesArr);
     }
 }
 
@@ -50,14 +78,10 @@ async function onWordInput() {
         const definitions = fullDict['definitions'];
         const synonyms = fullDict['synonyms'];
         const antonyms = fullDict['antonyms'];
-        const examples = fullDict['examples'];
         let resourcesArr = [definitions, synonyms, antonyms];
         resourcesArr = resourcesArr.filter(ele => ele.length > 0);
-        const randomResourceIndex = Math.floor(Math.random() * resourcesArr.length);
-        const selectedResource = resourcesArr[randomResourceIndex];
-        const selectedResourceIndex = 0;
         console.log('Let\'s play a game');
-        game(randomResourceIndex, selectedResource, selectedResourceIndex, word, resourcesArr);
+        await game(word, resourcesArr);
     } catch (error) {
         if (error instanceof EvalError)
             console.log(error.message);
